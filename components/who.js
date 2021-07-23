1ccd1b28692fd1066dda0a91f3047d3f8371dccf
bajpai244 <bajpaiharsh244@gmail.com>
2021-07-23 14:31:56 +0530

    chore: update eligibily on landing page

--- components/who.js
+++ components/who.js
@@ -7,13 +7,12 @@ const Who = ({ ...props }) => {
     <Container sx={{ variant: "cont", textAlign: "center" }}>
       <h2 sx={{ variant: "subt" }}>who is eligible to participate?</h2>
       <h1 sx={{ variant: "heading" }}>
-        Anyone from{" "}
-        <span sx={{ color: "blue", variant: "wavy" }}> Asia or Africa! </span>{" "}
+        Everyone! <span sx={{ color: "blue", variant: "wavy" }}> </span>{" "}
       </h1>
       <p sx={{ variant: "para" }}>
-        Any hacker who lives in Asia or Africa is eligible for this competition!
-        So if you have the hacker spirit and want to compete with other hackers
-        in your region, then what are you waiting for!{" "}
+        Any hacker who is in High School/University is eligible for this
+        competition! So if you have the hacker spirit and want to compete with
+        other hackers in your region, then what are you waiting for!{" "}
       </p>
       <Button as="a" href="./faq" sx={{ variant: "buttons.cta" }}>
         FAQs
